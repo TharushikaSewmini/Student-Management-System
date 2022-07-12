0d245b00152d6887f5d7e0dfffc988e8f8268fd4
Tharushika <tharushikasewmini@gmail.com>
2022-07-12 10:50:20 +0530

    Create DashBoardForm UI and added lib package

--- StudentManagementSystem/src/controller/DashBoardFormController.java
+++ StudentManagementSystem/src/controller/DashBoardFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DashBoardFormController {
+    public JFXTextField txtStudentId;
+    public JFXTextField txtName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+    public JFXButton btnSaveStudent;
+    public JFXButton btnDeleteCustomer;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void saveStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addStudentOnAction(ActionEvent actionEvent) {
+    }
 }
